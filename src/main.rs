e3cefdd3ddb8a690ebe3624640a44390933e4cf9
Sanzentyo <sanzen.tyo@gmail.com>
2026-06-01 20:56:01 +0900

    Add progress logging to renderer

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
-use std::num::NonZeroU16;
+use std::{borrow::Cow, num::NonZeroU16};
 
 use anyhow::Result;
+use indicatif::{ProgressBar, ProgressStyle};
 use raytracing_one_weekend_rs::pnm::{AsciiPpmBuf, Pnm};
+use tracing::info;
 
-fn to_u8(v: f64) -> u8 {
+const fn to_u8(v: f64) -> u8 {
     (v * 255.999).round() as u8
 }
 
@@ -12,6 +14,17 @@ fn main() -> Result<()> {
     const HEIGHT: usize = 256;
     const OUTPUT_DIR: &str = "output";
 
+    tracing_subscriber::fmt::init();
+
+    let spinner_style = ProgressStyle::default_bar();
+
+    let deps = (WIDTH * HEIGHT) as u64;
+    
+    let pb = ProgressBar::new(deps);
+    pb.set_style(spinner_style);
+
+
+    info!("Rendering image...");
     let mut image = AsciiPpmBuf::new(
         WIDTH,
         HEIGHT,
@@ -28,12 +41,16 @@ fn main() -> Result<()> {
             const B: f64 = 0.0;
 
             *pixel = [to_u8(r), to_u8(g), to_u8(B)];
+            pb.inc(1);
         });
+    pb.with_finish(indicatif::ProgressFinish::WithMessage(Cow::Borrowed("Image rendered.")));
 
     let pnm = Pnm::AsciiPpm(image);
 
+    info!("Saving image...");
     std::fs::create_dir_all(OUTPUT_DIR)?;
     pnm.save_with_extension(std::path::Path::new(OUTPUT_DIR).join("image"))?;
 
+    info!("Image saved {OUTPUT_DIR}/image.ppm");
     Ok(())
 }
--- src/vec.rs
+++ src/vec.rs
@@ -1,7 +1,8 @@
 use num_traits::Num;
+use std::fmt::Display;
 
 pub trait VecElem: Copy + Num {}
-impl<T: Copy + Num> VecElem for T {}
+impl<T: Copy + Num + Default + Display> VecElem for T {}
 
 #[derive(Debug, Clone, Copy)]
 #[repr(C, align(16))]
@@ -24,13 +25,48 @@ impl<T: VecElem> Default for Vec3<T> {
 }
 
 impl<T: VecElem> Vec3<T> {
-    pub fn new(x: T, y: T, z: T) -> Self {
+    pub const fn new(x: T, y: T, z: T) -> Self {
         Self {
             x,
             y,
             z,
             _padding: T::zero(),
         }
+    }
+
+    pub const fn zero() -> Self {
+        Self::default()
+    }
+
+    pub const fn one() -> Self {
+        Self::new(T::one(), T::one(), T::one())
+    }
+
+    pub const fn dot(self, rhs: Self) -> T {
+        self.x * rhs.x + self.y * rhs.y + self.z * rhs.z
+    }
+
+    pub const fn cross(self, rhs: Self) -> Self {
+        Self::new(
+            self.y * rhs.z - self.z * rhs.y,
+            self.z * rhs.x - self.x * rhs.z,
+            self.x * rhs.y - self.y * rhs.x
+        )
+    }
+
+    pub const fn length(self) -> T {
+        T::sqrt(self.dot(self))
+    }
+
+    pub const fn normalize(self) -> Self {
+        self / self.length()
+    }
+    
+}
+
+impl<T: VecElem> Display for Vec3<T> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}, {}, {}", self.x, self.y, self.z)
     }
 }
 
@@ -58,6 +94,20 @@ impl<T: VecElem> std::ops::Sub<Vec3<T>> for Vec3<T> {
         }
     }
 }
+
+impl<T: VecElem> std::ops::Mul<Vec3<T>> for Vec3<T> {
+    type Output = Vec3<T>;
+
+    fn mul(self, rhs: Vec3<T>) -> Vec3<T> {
+        Vec3 {
+            x: self.x * rhs.x,
+            y: self.y * rhs.y,
+            z: self.z * rhs.z,
+            _padding: T::zero(),
+        }
+    }
+}
+
 impl<T: VecElem> std::ops::Mul<T> for Vec3<T> {
     type Output = Vec3<T>;
 
@@ -81,4 +131,7 @@ impl<T: VecElem> std::ops::Div<T> for Vec3<T> {
             _padding: T::zero(),
         }
     }
+}
+
+impl<T: VecElem>  for Vec3<T> {
 }
